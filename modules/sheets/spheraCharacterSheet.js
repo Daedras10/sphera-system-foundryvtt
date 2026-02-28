938e772738a85259f3ef27d8d8ca2f27a38a2543
Arthur <35684550+Daedras10@users.noreply.github.com>
2026-02-28 21:44:30 +0100

    WIP: Characters

--- modules/sheets/spheraCharacterSheet.js
+++ modules/sheets/spheraCharacterSheet.js
@@ -1,9 +1,26 @@
-const api = foundry.applications.api;
-const sheets = foundry.applications.sheets;
-
-export default class SpheraCharacterSheet extends api.HandlebarsApplicationMixin(sheets.ActorSheetV2)
+export default class SpheraCharacterSheet extends ActorSheet
 {
-    sheetContext = {};
+    static get defaultOptions()
+    {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            templates: "systems/sphera/templates/sheets/actors/playerCharacter-sheet.hbs",
+            classes: ["sphera", "sheet", "actor", "playerCharacter"],
+        });
+    }
 
-    
+    getData(options = {}) {
+        const data = super.getData(options);
+
+        console.log("SPHERA | Item Character Data (initial info)", data);
+        
+        let sheetData = {
+            owner: this.actor.isOwner,
+            editable: this.isEditable,
+            system: data.actor.system,
+            config: CONFIG.sphera,
+            weapons: data.items.filter(function(item) { return item.type === "weapon" })
+        };
+        console.log("SPHERA | Item Character Data", sheetData);
+        return sheetData;
+    }
 }
